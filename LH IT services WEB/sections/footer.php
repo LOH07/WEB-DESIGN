63552a53d742c8fa3b61d728017b8240e0882c0f
Liviu Octavian Heinrich <76899634+LOH07@users.noreply.github.com>
2022-08-30 11:01:35 +0100

    Delete LH IT services WEB directory

--- LH IT services WEB/sections/footer.php
+++
@@ -1,8 +0,0 @@
-	<div id="footer">
-		<address id="copyright">
-			Copyright&#169; <?php include('functions/functions.php'); show_year(); ?>, Webmaster
-		</address>
-	</div>
-</div><!-- END of  #wrapper -->
-</body>
-</html>
